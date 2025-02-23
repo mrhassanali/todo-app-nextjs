ebd5bfb4883486075ac90c89e391a206e78433c1
Hassan Ali <bsf1905804@gmail.com>
2025-02-23 17:15:42 +0500

    remove unused DASHBOARD route import from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
-import { DASHBOARD, LOGIN } from "@/lib/constants/Route";
+import { LOGIN } from "@/lib/constants/Route";
 export { default } from "next-auth/middleware";
 
 // This function can be marked `async` if using `await` inside
