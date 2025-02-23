4f2e43520b7541f506390311e84c2162dfea8e1d
Hassan Ali <bsf1905804@gmail.com>
2025-02-23 16:32:12 +0500

    set dynamic rendering for todo page

--- src/app/dashboard/todo/page.tsx
+++ src/app/dashboard/todo/page.tsx
@@ -3,6 +3,8 @@ import { DataTable } from "@/components/todo/table";
 import { Todo } from "@prisma/client";
 import React from "react";
 
+export const dynamic = "force-dynamic";
+
 const page: React.FC = async () => {
   const todoList = (await getAllTodo()) as Todo[];
 
