5cae7d571dc61ace377ba0a0fc1df50679b4787b
Hassan Ali <bsf1905804@gmail.com>
2025-02-23 17:13:48 +0500

    adding middleware for auth

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+import { DASHBOARD, LOGIN } from "@/lib/constants/Route";
+export { default } from "next-auth/middleware";
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+  const session = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET!,
+  });
+
+  if (!session?.email) {
+    return NextResponse.redirect(new URL(LOGIN, request.url));
+  }
+
+  return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/dashboard/:path*"],
+};
